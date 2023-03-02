c03c509b07027790efb8cbc1cea0966c8f91a0c2
sagarapradeep <pradeepsyd54@gmail.com>
2023-03-02 14:44:06 +0530

    02/03/2023

---
+++ Phase-1/java-se/file-io/io-basics/src/main/java/lk/ijse/dep10/io/Test.java
@@ -0,0 +1,12 @@
+package lk.ijse.dep10.io;
+
+import java.io.File;
+
+public class Test {
+    public static void main(String[] args) {
+        File file = new File("/home/sagara/Desktop/def");
+//        System.out.println(file.length());
+        System.out.println("File length: " + file.length());
+
+    }
+}
